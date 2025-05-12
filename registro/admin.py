ef1a954c918197527f15dba8cf9bb6810437ae21
victorcav <victor.bcav@gmail.com>
2025-05-12 20:30:57 -0300

    Criação dos Modelos

--- registro/admin.py
+++ registro/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Funcionario, ColetaFaces, Treinamento
+
+class ColetaFacesInline(admin.StackedInline):
+    model = ColetaFaces
+    extra = 0
+
+class FuncionarioAdmin(admin.ModelAdmin):
+    readonly_fields = ['slug']
+    inlines = (ColetaFacesInline,)
+
+admin.site.register(Funcionario, FuncionarioAdmin)
+admin.site.register(Treinamento)
 
 # Register your models here.
--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.text import slugify
+from random import randint
 
-# Create your models here.
+class Funcionario(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+    foto = models.ImageField(upload_to='foto/')
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+    
+    def save(self, *args, **kwargs):
+        seq = self.nome + '_FUNC' + str(randint(1000000, 9999999))
+        self.slug = slugify(seq)
+        super().save(*args, **kwargs)
+
+class ColetaFaces(models.Model):
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.CASCADE, related_name='funcionario_coletas')
+    image = models.ImageField(upload_to='roi/')
+
+class Treinamento(models.Model):
+    modelo = models.FileField(upload_to='treinamento/')
+
+    class Meta:
+        verbose_name = 'Treinamento'
+        verbose_name_plural = 'Treinamentos'
+
+    def __str__(self):
+        return 'Classificador (frontalface)'
+    
+    def clean(self):
+        model = self.__class__
+        if model.objects.exclude(id=self.id).exists():
+            raise ValidationError("Só pode haver 1 arquivo salvo")
+
+
+
+
+
