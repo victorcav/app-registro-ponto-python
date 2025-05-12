78009715b4b9aa7ab375fb9b6e7667987a5d8b8f
victorcav <victor.bcav@gmail.com>
2025-05-12 20:04:42 -0300

    Iniciando aplicação com o Template Base

--- gestao/settings.py
+++ gestao/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'registro'
 ]
 
 MIDDLEWARE = [
@@ -81,10 +83,9 @@ WSGI_APPLICATION = 'gestao.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
